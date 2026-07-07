fc53a23cdd910201e09516109fcdce89a3d1765d
HarshaVardhanVarma <hvarma234@gmail.com>
2026-07-07 16:28:13 +0530

    fixed db errors

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,11 +1,20 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
+import { getDatabaseUrl } from "./databaseUrl.js";
 
 const globalForPrisma = globalThis;
+const databaseUrl = getDatabaseUrl();
 
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    ...(databaseUrl
+      ? {
+          datasources: {
+            db: { url: databaseUrl },
+          },
+        }
+      : {}),
     log: process.env.NODE_ENV === "development" ? ["warn", "error"] : ["error"],
   });
 
